132a1122a2edb689350654e0735906a9ca931a2b
Brodie969 <131664136+Brodie969@users.noreply.github.com>
2023-10-03 14:34:39 +1000

    Big Commit

--- app.py
+++ app.py
@@ -2,10 +2,19 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("home.html")
 
 # @app.route('/user/<name>')
 # def user(name):
 #   return render_template('user.html', name=name)
+
+@app.route("/countdowns")
+def countdown():
+    return render_template("countdown.html")
+
+@app.route("/todo")
+def todo():
+    # Get items from text file on server and add them to template variables
+    return render_template("todo.html")
---
+++ static/countdown.js
@@ -0,0 +1,4 @@
+app = document.getElementById("app");
+app.innerHTML = ""; // Clear Loading
+
+app.innerHTML = `<h3>Time Until ${date}:`
