b31966cc51373692ac8fce9ab3502cbe1599b165
Brodie969 <131664136+Brodie969@users.noreply.github.com>
2023-10-07 09:05:27 +1000

    Working File Fetch

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("home.html")
+
+@app.route("/fetch")
+def fetch():
+    return send_from_directory("data", "reminders.txt")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
--- static/reminders.js
+++ static/reminders.js
@@ -5,6 +5,34 @@ Python can take that raw data and place it in the file, in chronological order
 
 There will also be a delete button next to every reminder/countdown to be handled by Javscript */
 
+// I will refactor everything to countdown later (maybe)
+
+// Change Title And Remove "Loading"
+const formHead = document.getElementById("remindersFormStatic");
+formHead.innerHTML = `<h2>Add A Reminder:</h2>`;
+
+const head = document.getElementById("remindersStatic");
+head.innerHTML = `<h2>Reminders:</h2>`;
+
+// This is where the txt will be fetched and added to #reminders
+function getReminders() {
+    fetch("/fetch")
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.text(); // Get the text content of the file
+    })
+    .then(text => {
+        // Do something with the text content, for example, display it in a <pre> element
+        console.log(text);
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
+}
+
+
 // Load Form
 const formElement = document.getElementById("remindersForm");
 formElement.innerHTML = `<form id="create"><label for="name">Name:</label><input type="text" id="name" required><br><br><label for="date">Date:</label><input type="date" id="date" required><br><br><button type="submit">Submit</button></form>`;
@@ -17,4 +45,4 @@ form.addEventListener("submit", function(event) {
     alert("Working");
 });
 
-
+getReminders();
