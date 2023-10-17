062eb45af08f3bf7c2c9115bf3895c1a1989678d
Brodie969 <131664136+Brodie969@users.noreply.github.com>
2023-10-17 14:06:21 +1000

    Changed Project Structure

--- app.py
+++ app.py
@@ -5,44 +5,5 @@ app = Flask(__name__)
 def index():
     return render_template("home.html")
 
-@app.route("/data")
-def data():
-    return send_from_directory("data", "reminders.txt")
-
-@app.route("/sync")
-def sync():
-    return render_template("file.html")
-
-@app.route("/update", methods=["POST"])
-def update():
-    data = request.get_json()  # Assuming you send data as JSON
-    if data:
-        with open("data/reminders.txt", "a") as file:
-            file.write(data + "\n")
-        return "Data appended successfully"
-    else:
-        return "No data received"
-
-@app.route("/delete", methods=["POST"])
-def delete():
-    data = request.get_json()
-    print(data)
-    index = data.get('index')
-    if index:
-        index = int(index)
-        lines = []
-        with open("data/reminders.txt", "r") as file:
-            lines = file.readlines()
-        if len(lines) >= index:
-            del lines[index - 1]
-            with open("data/reminders.txt", "w") as file:
-                file.writelines(lines)
-            return "File Updated Sucessfully"
-        else:
-            return "File Was Not Updated"
-    else:
-        return "Invalid Input"
-        
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
